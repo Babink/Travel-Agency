78bdb3b5dbef52f51b7fbac40ccf7922acfa5e4a
babin khanal <30598319+Babink@users.noreply.github.com>
2020-01-22 00:26:42 +0545

    error fixed

--- lib/travel_web/controllers/auth_controller.ex
+++ lib/travel_web/controllers/auth_controller.ex
@@ -3,6 +3,7 @@ defmodule TravelWeb.AuthController do
 
     alias TravelWeb.User
     alias Travel.Repo
+    import Ecto.Query
 
 def create_acc(conn , _params) do
     changeset = User.changeset(%User{} , %{})
@@ -11,23 +12,34 @@ end
 
 
 def signup(conn , %{"user" => %{"name" => user_name , "email" => user_email , "password" => user_password}}) do
-     if String.length(user_email) >= 5 do
-        user_password = Base.encode16(user_password)
-        changeset = User.changeset(%User{} , %{name: user_name , email: user_email , password: user_password})
+     if String.length(user_email) >= 5 && String.length(user_password) >= 5 && String.length(user_name) >= 2 do
+        isEmailCorrect = String.contains?(user_email , ["@" , "gmail" , "com"])
+
+
+        case isEmailCorrect do
+            true ->
+                user_password = Base.encode16(user_password)
+                changeset = User.changeset(%User{} , %{name: user_name , email: user_email , password: user_password})
         
-        case Repo.insert(changeset) do
-            {:ok , cut} -> 
+                case Repo.insert(changeset) do
+                    {:ok , cut} -> 
+                        conn
+                        |> put_session(:user , cut.id)
+                        |> put_flash(:info , "Successfully registered user")
+                        |> redirect(to: Routes.home_path(conn , :index))
+        
+            
+                    {:error , changeset} ->
+                     put_flash(conn , :info , "Error while registering user")
+                     render conn , "signup.html" , changeset: changeset  
+            
+                end 
+
+            false ->
                 conn
-                    |> put_session(:user , cut.id)
-                    |> put_flash(:info , "Successfully registered user")
-                    |> redirect(to: Routes.home_path(conn , :index))
-        
-            
-            {:error , changeset} ->
-                put_flash(conn , :info , "Error while registering user")
-                render conn , "signup.html" , changeset: changeset  
-            
-            end 
+                |> put_flash(:info , "Please enter valid email address")
+                |> redirect(to: Routes.auth_path(conn , :create_acc))
+        end 
     else
         conn
         |> put_flash(:info , "Please fill the form first")
@@ -44,21 +56,32 @@ end
 
 
 def login(conn , %{"user" => %{ "email" => user_email , "password" => user_password }}) do
-    if String.length(user_email) >= 5 do
-        find_email = Repo.get_by!(User , email: user_email)
-        {:ok , new_password} = Base.decode16(find_email.password)
+    if String.length(user_email) >= 5 && String.length(user_password) >= 5 do
 
-        cond do
-            new_password == user_password ->
+        query = from i in User , where: i.email == ^user_email , select: i
+        return_val = Repo.all(query)
+
+        case length(return_val) do
+            0 ->
                 conn
-                |> put_session(:user , find_email.id)
-                |> put_flash(:info , "Successfully registered user")
-                |> redirect(to: Routes.home_path(conn , :index))
-            true -> 
-                conn
-                |> put_flash(:error , "Error while logging user")
-                "False"
-            end
+                |> put_flash(:info , "Incorrect email address.")
+                |> redirect(to: Routes.auth_path(conn , :login_page))
+            
+            1 -> 
+                find_email = Repo.get_by!(User , email: user_email)
+                {:ok , new_password} = Base.decode16(find_email.password)
+                cond do
+                    new_password == user_password ->
+                        conn
+                        |> put_session(:user , find_email.id)
+                        |> put_flash(:info , "Successfully registered user")
+                        |> redirect(to: Routes.home_path(conn , :index))
+                    new_password != user_password ->
+                        conn
+                        |> put_flash(:info , "Password is incorrect")
+                        |> redirect(to: Routes.auth_path(conn , :login_page))
+                    end
+        end
     else
         conn
         |> put_flash(:info , "Please enter required field")
